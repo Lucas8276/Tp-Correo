45f6462fe38f915c4d7dc997300959a6944331dd
GitHub Action <action@github.com>
2023-10-09 20:18:23 +0000

    Add generated files

--- src/main/java/gestorcito/interfac/FIltro_Base.java
+++
@@ -1,9 +0,0 @@
-package gestorcito.interfac;
-
-import java.util.List;
-import gestorcito.objetos.Mensaje;
-public interface FIltro_Base {
-    List<Mensaje> aplicoFiltro(List <Mensaje>mensajes);
-}
-
-
